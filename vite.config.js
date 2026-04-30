8ba5d047bdd4397f0bbdbd172e5499f71b4fa265
Patrik Pašek <pasekpat@student.cvut.cz>
2026-04-30 11:28:27 +0200

    [Cof] config vite-pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,43 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss()
-  ],
+    plugins: [
+        tailwindcss(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: 'auto',
+            devOptions: {
+                enabled: true
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+                cleanupOutdatedCaches: true,
+            },
+
+            includeAssets: ['earth.svg', 'earth-bg.png', 'robots.txt'],
+
+            manifest: {
+                name: 'GeoHelper',
+                short_name: 'GeoHelper',
+                theme_color: '#ffffff',
+                background_color: '#ffffff',
+                display: 'standalone',
+                icons: [
+                    {
+                        src: 'earth-bg.png', 
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'earth-bg.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable' 
+                    }
+                ]
+            }
+        })
+    ],
 });
