3b041f322830ac1d395b4287bfe4457387af9776
Patrik Pašek <pasekpat@student.cvut.cz>
2026-05-10 14:18:38 +0200

    [Fix] responsive warning

--- src/pages/Catching.ts
+++ src/pages/Catching.ts
@@ -128,7 +128,7 @@ export class Catching extends Page {
             <h2 class="text-5xl font-bold m-4">${this.catching?.name}</h2>
             <map-help></map-help>
 
-            <section class="w-sm flex flex-col ml-8 mt-4 mb-8 md:flex-row">
+            <section class="flex flex-col ml-8 mt-4 mb-8 md:flex-row">
 
                 <div>
                     <form id="form-lat-log">
@@ -153,7 +153,7 @@ export class Catching extends Page {
                             />
                         </fieldset>
 
-                        <div class="warning bg-orange-200 px-6 py-4 my-4 rounded-md text-sm">
+                        <div class="warning w-full bg-orange-200 px-6 py-4 my-4 rounded-md text-sm">
                             <svg viewBox="0 0 24 24" class="text-yellow-600 w-5 h-5 sm:w-5 sm:h-5 mr-3">
                                 <path fill="currentColor"
                                     d="M23.119,20,13.772,2.15h0a2,2,0,0,0-3.543,0L.881,20a2,2,0,0,0,1.772,2.928H21.347A2,2,0,0,0,23.119,20ZM11,8.423a1,1,0,0,1,2,0v6a1,1,0,1,1-2,0Zm1.05,11.51h-.028a1.528,1.528,0,0,1-1.522-1.47,1.476,1.476,0,0,1,1.448-1.53h.028A1.527,1.527,0,0,1,13.5,18.4,1.475,1.475,0,0,1,12.05,19.933Z">
